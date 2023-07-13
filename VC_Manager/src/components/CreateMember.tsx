cde774cb7b6a648999ecfdadb5c719fcdce6bfea
Zarna Pathak <pathakzarna0128@gmail.com>
2023-07-13 14:15:09 +0530

    Zarna/CreateMember/Add Member logic created

--- VC_Manager/src/components/CreateMember.tsx
+++ VC_Manager/src/components/CreateMember.tsx
@@ -1,4 +1,49 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, database } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+interface memberData {
+  firstName: string;
+  lastName: string;
+  amount: number;
+}
+
 export const CreateMember = () => {
+  const memberRef = collection(database, "Members");
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  const memberSchema = yup.object().shape({
+    firstName: yup.string().required("You Must Add First Name."),
+    lastName: yup.string().required("You Must Add Last Name."),
+    amount: yup
+      .number()
+      .typeError("Amount must be a Number")
+      .required("You Must Add Ammount and it should be only Numbers.")
+      .min(1),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(memberSchema),
+  });
+
+  const addOnSubmit = async (data: memberData) => {
+    await addDoc(memberRef, {
+      ...data,
+      userId:user?.uid
+    });
+    navigate("/");
+    console.log(data);
+  };
+
   return (
     <>
       <br />
@@ -9,15 +54,16 @@ export const CreateMember = () => {
         >
           <h2 className="card-title text-center">Enter Member Details</h2>
           <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit(addOnSubmit)}>
               <div className="form-outline mb-4">
                 <p className="form-label fs-5 text-start">First Name</p>
                 <input
                   type="text"
                   id="form4Example1"
                   className="form-control"
+                  {...register("firstName")}
                 />
-                <p style={{ color: "red" }}></p>
+                <p style={{ color: "red" }}>{errors.firstName?.message}</p>
               </div>
               <div className="form-outline mb-4">
                 <p className="form-label fs-5 text-start">Last Name</p>
@@ -25,8 +71,9 @@ export const CreateMember = () => {
                   type="text"
                   id="form4Example1"
                   className="form-control"
+                  {...register("lastName")}
                 />
-                <p style={{ color: "red" }}></p>
+                <p style={{ color: "red" }}>{errors.lastName?.message}</p>
               </div>
               <div className="form-outline mb-4">
                 <p className="form-label fs-5 text-start">Ammount</p>
@@ -34,16 +81,9 @@ export const CreateMember = () => {
                   type="text"
                   id="form4Example1"
                   className="form-control"
+                  {...register("amount")}
                 />
-                <p style={{ color: "red" }}></p>
-              </div>
-              <div className="form-outline mb-4">
-                <p className="form-label fs-5 text-start">Discription</p>
-                <textarea
-                  className="form-control"
-                  id="form4Example3"
-                ></textarea>
-                <p style={{ color: "red" }}></p>
+                <p style={{ color: "red" }}>{errors.amount?.message}</p>
               </div>
               <input
                 type="submit"
--- VC_Manager/src/config/firebase.ts
+++ VC_Manager/src/config/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,10 +12,11 @@ const firebaseConfig = {
   projectId: "vc-manager-ada83",
   storageBucket: "vc-manager-ada83.appspot.com",
   messagingSenderId: "971637236172",
-  appId: "1:971637236172:web:8eba2c57d093d262fd7d69"
+  appId: "1:971637236172:web:8eba2c57d093d262fd7d69",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const database = getFirestore(app);
